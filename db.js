04d779191c0f36142e6969c8bd1818878d52974c
KRISHNENDU BIR <23EE01025@iitbbs.ac.in>
2024-12-19 19:46:41 +0530

    change

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb+srv://23ee01025:Krishnendu123@cluster0.f4nm2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/gym-database"
+const mongoURI="mongodb+srv://23ee01025:Krishnendu123@cluster0.f4nm2.mongodb.net/gym-data?retryWrites=true&w=majority&appName=Cluster0/"
 
 // Function to connect to MongoDB database
 const ConnectToMongo=()=>{
